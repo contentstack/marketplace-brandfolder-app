7abe3c05d3b3da5111722a9573b6253014720fb9
saima <saima.sayed@contentstack.com>
2023-06-15 12:29:26 +0530

    resolved PR comments

--- ui/src/services/index.ts
+++ ui/src/services/index.ts
@@ -4,18 +4,17 @@
 /* Import node module CSS */
 /* Import our CSS */
 
+import constants from "../common/constants";
+
 const checkConfigValidity = async (apiKey: any) => {
-  let response: any = await fetch(
-    "https://brandfolder.com/api/v4/brandfolders",
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-    }
-  );
+  let response: any = await fetch(constants.brandfolderUrl, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+  });
   response = await response.json();
   // eslint-disable-next-line
   return response?.data?.length ? true : false;
