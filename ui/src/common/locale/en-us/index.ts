8f0b9739077e82d5797c28e7753f284b22f04bfc
roopkadam <37994191+roopkadam@users.noreply.github.com>
2024-05-07 15:53:46 +0530

    locale text updated

--- ui/src/common/locale/en-us/index.ts
+++ ui/src/common/locale/en-us/index.ts
@@ -23,6 +23,17 @@ const localeTexts = {
       help: "Select the keys you want to save",
       placeholder: "Select keys",
       instruction: "Select the keys you want to save",
+      invalidCredentials: "Invalid Configuration",
+      isExtension: {
+        label: "Enable Extension Support",
+        instruction:
+          "If this toggle is enabled, you will be able to get the asset JSON data similar to the Brandfolder Extension",
+        legacyInfo:
+          "Legacy settings allow you to use the Brandfolder app with Extension support, enabling you to get asset JSON data similar to that of the Brandfolder Extension. However, if you prefer to use the latest Brandfolder app, you can skip configuring the Legacy settings.",
+        legacy_title: "Legacy Settings",
+        warning_note:
+          "Note: When you toggle between App and Extension settings, please note that the data variation may occur.",
+      },
     },
     customWholeJson: {
       modal: {
@@ -53,6 +64,7 @@ const localeTexts = {
     invalidCredentials: "Invalid Configuration",
     emptyValue: "Field Value Missing",
   },
+
   CustomFields: {
     assetLimit: {
       btnTooltip:
