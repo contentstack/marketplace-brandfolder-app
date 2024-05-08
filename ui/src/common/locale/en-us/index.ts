15881894f793625f1c6273b29b1d9abc5e9a4a7c
roopkadam <37994191+roopkadam@users.noreply.github.com>
2024-05-08 11:51:24 +0530

    locale text updated

--- ui/src/common/locale/en-us/index.ts
+++ ui/src/common/locale/en-us/index.ts
@@ -2,6 +2,16 @@ import rootConfig from "../../../root_config";
 
 const localeTexts = {
   ConfigFields: {
+    isExtension: {
+      label: "Enable Extension Support",
+      instruction:
+        "If this toggle is enabled, you will be able to get the asset JSON data similar to the Brandfolder Extension",
+      legacyInfo:
+        "Legacy settings allow you to use the Brandfolder app with Extension support, enabling you to get asset JSON data similar to that of the Brandfolder Extension. However, if you prefer to use the latest Brandfolder app, you can skip configuring the Legacy settings.",
+      legacy_title: "Legacy Settings",
+      warning_note:
+        "Note: When you toggle between App and Extension settings, please note that the data variation may occur.",
+    },
     entrySaveRadioButton: {
       label: "Save In Entry",
       help: `You can select how you want to save the data you get from ${rootConfig.damEnv.DAM_APP_NAME}.`,
@@ -35,6 +45,7 @@ const localeTexts = {
           "Note: When you toggle between App and Extension settings, please note that the data variation may occur.",
       },
     },
+
     customWholeJson: {
       modal: {
         header: "Add Key Path",
