3936da100fa9ad02e8fe32227bd06a9e32c3163d
Shradha Mishra <shradhamishra2224@gmail.com>
2025-02-25 15:11:59 +0530

    feat(rte): enhance RTE component functionality and UI

--- ui/rte/src/common/locale/en-us/index.ts
+++ ui/rte/src/common/locale/en-us/index.ts
@@ -7,7 +7,7 @@ const localeTexts = {
   RTE: {
     ToolTip: {
       viewIcon: "Preview",
-      openInDAM: `Open in ${rteConfig?.damEnv?.DAM_APP_NAME}`,
+      openInDAM: `Open In ${rteConfig?.damEnv?.DAM_APP_NAME}`,
     },
     title: `Choose assets from ${rteConfig?.damEnv?.DAM_APP_NAME}`,
     button: {
@@ -18,13 +18,19 @@ const localeTexts = {
       remove: "Remove",
       edit: "Edit Properties",
       preview: "Preview Asset",
-      openInDAM: `Open in ${rteConfig?.damEnv?.DAM_APP_NAME}`,
+      openInDAM: `Open In ${rteConfig?.damEnv?.DAM_APP_NAME}`,
+    },
+    assetValidation: {
+      errorStatement:
+        "Error: $var cannot be added as it does not match the asset constraints provided.",
+      configDeletedImg:
+        "Unable to access the image url. The link might be broken, the asset may have been deleted, or you do not have access to it.",
     },
   },
   DeleteModal: {
     header: "Remove Asset from Contentstack Entry",
-    bodyBeforePlaceholder: "Are you sure you want to remove  ",
-    bodyAfterPlaceholder: " from Contentstack Entry?",
+    body: "Are you sure you want to remove <b>&apos;$&apos;</b> from Contentstack Entry?",
+    textPlaceholder: "Enter asset name for confirmation",
     cancelButton: "Cancel",
     confirmButton: "Remove",
   },
@@ -33,6 +39,14 @@ const localeTexts = {
     audio: "Edit Audio Properties",
     image: "Edit Image Properties",
     default: "Edit Asset Properties",
+  },
+  Icons: {
+    embed: "Embed",
+    dontSave: "DontSave",
+    imageSettings: "ImageSettings",
+    addPlus: "AddPlus",
+    saveWhite: "SaveWhite",
+    removeFilled: "RemoveFilled",
   },
 };
 
